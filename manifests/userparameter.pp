055363c22164023b1771477255c3bc77ba033732
Werner Dijkerman <ikben@werner-dijkerman.nl>
2014-12-05 21:06:13 +0100

    Added userparameter class for hiera or the foreman users

---
+++ manifests/userparameter.pp
@@ -0,0 +1,35 @@
+# == Class: zabbix::userparameter
+#
+# This class can be used when you use hiera or The Foreman. With this tools
+# you can't use and define. This make use of "create_resources".
+#
+# == Requirements
+#
+# Hiera or The Foreman
+#
+# == Parameters
+#
+# [*data*]
+#  This is the data in YAML format
+# 
+# == Example
+#
+# zabbix::userparameter::data:
+#  MySQL:
+#    content: UserParameter=mysql.ping,mysqladmin -uroot ping | grep -c alive
+#
+#
+# === Authors
+#
+# Author Name: ikben@werner-dijkerman.nl
+#
+# === Copyright
+#
+# Copyright 2014 Werner Dijkerman
+#
+
+class zabbix::userparameter (
+  $data = '',
+) {
+  create_resources('zabbix::userparameters', $data)
+}
