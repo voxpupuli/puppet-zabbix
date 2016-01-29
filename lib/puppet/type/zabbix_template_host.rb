69bfd6798bad23f2cf59b1d61f9a380485408412
hkumarmk <hkumar@d4devops.org>
2016-01-29 21:36:40 +0000

    Added zabbix_template_host type
    This new type can be used to link/unlink template to the host. zabbix_host
    provide a way to assign list of templates while creating the host. But in
    a scenario where:
    * One need to add zabbix_host from agent setup code
    * Link appropriate templates from the service setup code. For example, mysql server
    deployment code can use below code to link the template "Template App MySQL" to the host.
    
    ```
    zabbix_template_host{"Template App MySQL@${::hostname}": }
    
    ```

---
+++ lib/puppet/type/zabbix_template_host.rb
@@ -0,0 +1,34 @@
+require 'puppet/util/zabbix'
+
+Puppet::Type.newtype(:zabbix_template_host) do
+
+  @doc = %q{Link or Unlink template to host.
+	  Example.
+	  Name should be in the format of "template_name@hostname"
+
+	  zabbix_template_host{"mysql_template@db1":
+            ensure => present
+          }
+  }
+
+  ensurable do
+    defaultvalues
+    defaultto :present
+  end
+
+  newparam(:name, :namevar => true) do
+      newvalues(/.+\@.+/)
+      desc 'template_name@host_name'
+  end
+
+Puppet::Util::Zabbix.add_zabbix_type_methods(self)
+
+  autorequire(:zabbix_host) do
+    self[:name].split('@')[1]
+  end
+
+  autorequire(:zabbix_template) do
+    self[:name].split('@')[0]
+  end
+
+end
