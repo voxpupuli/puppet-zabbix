ce27149cb75e90ec8f591b0c45c9f95b06222940
Eric Anderson <eric_anderson2@cable.comcast.com>
2016-02-18 22:20:39 -0800

    fixed the unless check

--- manifests/server.pp
+++ manifests/server.pp
@@ -435,7 +435,7 @@ class zabbix::server (
     exec { "prevent zabbix boot-start":
       path    => "/usr/bin:/usr/sbin:/bin",
       command => "systemctl disable ${server_service_name}",
-      unless  => "systemctl is-enabled ${server_service_name}; echo $?",
+      unless  => "systemctl is-enabled ${server_service_name}| grep enabled; echo $?",
     }
 
     service { $server_service_name:
