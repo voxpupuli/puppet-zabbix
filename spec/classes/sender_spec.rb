a52553f793e657dec5756a9450922ef569c34c38
Tim Meusel <tim@bastelfreak.de>
2016-04-09 13:25:51 +0200

    add missing facts in sender_spec
    Signed-off-by: Tim Meusel <tim@bastelfreak.de>

--- spec/classes/sender_spec.rb
+++ spec/classes/sender_spec.rb
@@ -6,12 +6,21 @@ describe 'zabbix::sender' do
   context 'On RedHat 7.1' do
     let (:facts) do
       {
-        :osfamily               => 'RedHat',
-        :operatingsystem        => 'RedHat',
-        :operatingsystemrelease => '7.1',
-        :architecture           => 'x86_64',
-        :lsbdistid              => 'RedHat',
-        :concat_basedir         => '/tmp',
+        :osfamily                   => 'RedHat',
+        :operatingsystem            => 'RedHat',
+        :operatingsystemrelease     => '7.1',
+        :operatingsystemmajrelease  => '7',
+        :architecture               => 'x86_64',
+        :lsbdistid                  => 'RedHat',
+        :concat_basedir             => '/tmp',
+        :is_pe                      => false,
+        :puppetversion              => Puppet.version,
+        :facterversion              => Facter.version,
+        :ipaddress                  => '192.168.1.10',
+        :lsbdistcodename            => '',
+        :id                         => 'root',
+        :kernel                     => 'Linux',
+        :path                       => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/sbin',
       }
     end
 
