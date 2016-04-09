50b66361ea3ec5c6a02fc25993441161fec7ca20
Tim Meusel <tim@bastelfreak.de>
2016-04-09 13:26:32 +0200

    add facts in userparameter_spec
    Signed-off-by: Tim Meusel <tim@bastelfreak.de>

--- spec/classes/userparameter_spec.rb
+++ spec/classes/userparameter_spec.rb
@@ -2,6 +2,26 @@ require 'spec_helper'
 
 describe 'zabbix::userparameter' do
   let (:node) { 'agent.example.com' }
-
-  it { should compile }
+  context 'On RedHat 7.1' do
+    let (:facts) do
+      {
+        :osfamily                   => 'RedHat',
+        :operatingsystem            => 'RedHat',
+        :operatingsystemrelease     => '7.1',
+        :operatingsystemmajrelease  => '7',
+        :architecture               => 'x86_64',
+        :lsbdistid                  => 'RedHat',
+        :concat_basedir             => '/tmp',
+        :is_pe                      => false,
+        :puppetversion              => Puppet.version,
+        :facterversion              => Facter.version,
+        :ipaddress                  => '192.168.1.10',
+        :lsbdistcodename            => '',
+        :id                         => 'root',
+        :kernel                     => 'Linux',
+        :path                       => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/sbin',
+      }
+    end
+    it { should compile }
+  end
 end
