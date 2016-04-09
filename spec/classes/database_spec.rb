c57886acf35571c34f560c0e138cfb41dc14d076
Tim Meusel <tim@bastelfreak.de>
2016-04-09 13:23:27 +0200

    add missing facts in database_spec
    Signed-off-by: Tim Meusel <tim@bastelfreak.de>

--- spec/classes/database_spec.rb
+++ spec/classes/database_spec.rb
@@ -10,16 +10,21 @@ describe 'zabbix::database' do
   context 'On RedHat 6.5' do
     let (:facts) do
       {
-        :kernel                    => 'Linux',
-        :osfamily                  => 'RedHat',
-        :operatingsystem           => 'RedHat',
-        :operatingsystemrelease    => '6.5',
-        :operatingsystemmajrelease => '6',
-        :architecture              => 'x86_64',
-        :lsbdistid                 => 'RedHat',
-        :concat_basedir            => '/tmp',
-        :puppetversion             => '4.0.0',
-        :id                        => 'root',
+        :osfamily                   => 'RedHat',
+        :operatingsystem            => 'RedHat',
+        :operatingsystemrelease     => '6.5',
+        :operatingsystemmajrelease  => '6',
+        :architecture               => 'x86_64',
+        :lsbdistid                  => 'RedHat',
+        :concat_basedir             => '/tmp',
+        :is_pe                      => false,
+        :puppetversion              => Puppet.version,
+        :facterversion              => Facter.version,
+        :ipaddress                  => '192.168.1.10',
+        :lsbdistcodename            => '',
+        :id                         => 'root',
+        :kernel                     => 'Linux',
+        :path                       => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/sbin',
       }
     end
 
