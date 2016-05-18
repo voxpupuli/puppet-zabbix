e0b52ddb47a3a4650989936c2cd0f89622a91c0c
ITler <itl@netterchef.de>
2016-05-18 09:15:54 +0200

    Make module run with ubuntu.

--- manifests/server.pp
+++ manifests/server.pp
@@ -507,7 +507,7 @@ class zabbix::server (
   }
 
   # check if selinux is active and allow zabbix
-  if $::selinux_config_mode == 'enforcing' {
+  if defined(selinux_config_mode) and $::selinux_config_mode == 'enforcing' {
     selboolean{'zabbix_can_network':
       persistent => true,
       value      => 'on',
