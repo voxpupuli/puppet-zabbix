5522a14538053dbdbb701a5514ce36d2d30e5c42
Tim Meusel <tim@bastelfreak.de>
2016-05-21 12:55:48 +0200

    modulesync 0.6.2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
-require 'spec_helper_methods'
 include RspecPuppetFacts
+require 'spec_helper_methods'
+# vim: syntax=ruby
