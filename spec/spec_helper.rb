54cac20024a243a4a67d589870beaf6ae4b4f3fc
Tim Meusel <tim@bastelfreak.de>
2016-06-13 15:51:28 +0200

    rubocop: fix Style/TrailingCommaInLiteral

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,17 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
+
+RSpec.configure do |c|
+  c.default_facts = {
+    concat_basedir: '/tmp',
+    is_pe: false,
+    selinux_config_mode: 'disabled',
+    puppetversion: Puppet.version,
+    facterversion: Facter.version,
+    ipaddress: '172.16.254.254',
+    macaddress: 'AA:AA:AA:AA:AA:AA'
+  }
+end
 require 'spec_helper_methods'
 # vim: syntax=ruby
