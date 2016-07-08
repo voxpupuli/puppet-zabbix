6013e47e82bc54ecc8ec1b10de668c2c5fe0f2c5
Administrator <admin@example.com>
2016-07-08 17:10:55 +0300

    Adds manage_service param

--- manifests/server.pp
+++ manifests/server.pp
@@ -32,6 +32,11 @@
 # [*manage_firewall*]
 #   When true, it will create iptables rules.
 #
+# [*manage_service*]
+#   When true, it will ensure service running and enabled.
+#   When false, it does not care about service
+#   Default: true
+#
 # [*server_configfile_path*]
 #   Server config file path defaults to /etc/zabbix/zabbix_server.conf
 #
@@ -286,6 +291,7 @@ class zabbix::server (
   $manage_firewall         = $zabbix::params::manage_firewall,
   $manage_repo             = $zabbix::params::manage_repo,
   $manage_database         = $zabbix::params::manage_database,
+  $manage_service          = $zabbix::params::manage_service,
   $server_configfile_path  = $zabbix::params::server_configfile_path,
   $server_config_owner     = $zabbix::params::server_config_owner,
   $server_config_group     = $zabbix::params::server_config_group,
@@ -425,7 +431,7 @@ class zabbix::server (
 
   # Workaround for: The redhat provider can not handle attribute enable
   # This is only happening when using an redhat family version 5.x.
-  if $::osfamily == 'redhat' and $::operatingsystemrelease !~ /^5.*/ {
+  if $::osfamily == 'redhat' and $::operatingsystemrelease !~ /^5.*/ and $manage_service {
     Service[$server_service_name] {
       enable => true }
   }
@@ -461,15 +467,18 @@ class zabbix::server (
         ],
     }
   } else {
-    service { $server_service_name:
-      ensure     => running,
-      hasstatus  => true,
-      hasrestart => true,
-      require    => [
-        Package["zabbix-server-${db}"],
-        File[$include_dir],
-        File[$server_configfile_path],
-        ],
+    if $manage_service {
+      service { $server_service_name:
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        require    => [
+          Package["zabbix-server-${db}"],
+          File[$include_dir],
+          File[$server_configfile_path],
+          ],
+        subscribe  => File[$server_configfile_path],
+      }
     }
   }
 
@@ -479,7 +488,6 @@ class zabbix::server (
     owner   => $server_config_owner,
     group   => $server_config_group,
     mode    => '0640',
-    notify  => Service[$server_service_name],
     require => Package["zabbix-server-${db}"],
     replace => true,
     content => template('zabbix/zabbix_server.conf.erb'),
