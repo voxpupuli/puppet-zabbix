2917d0aac9828b235295323ea1111b60cfac78f9
Tim Meusel <tim@bastelfreak.de>
2017-05-21 14:33:06 +0200

    enhance acceptance testing

--- spec/acceptance/agent_spec.rb
+++ spec/acceptance/agent_spec.rb
@@ -21,7 +21,8 @@ describe 'zabbix::agent class' do
     end
 
     describe service('zabbix-agent') do
-      it { is_expected.to be_running }
+      it { is_expected.to be_running.under('systemd') }
+      it { is_expected.to be_enabled }
     end
   end
 end
