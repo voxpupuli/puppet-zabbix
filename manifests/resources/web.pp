dbd54371375f400fff4cf3268a342629303b7ee4
Francois Laupretre <francois@tekwire.net>
2017-07-03 20:28:13 +0200

    Fix tabs

--- manifests/resources/web.pp
+++ manifests/resources/web.pp
@@ -46,7 +46,7 @@ class zabbix::resources::web (
     zabbix_user    => $zabbix_user,
     zabbix_pass    => $zabbix_pass,
     apache_use_ssl => $apache_use_ssl,
- }
+  }
   -> Zabbix_userparameters <<| |>> {
     zabbix_url     => $zabbix_url,
     zabbix_user    => $zabbix_user,
