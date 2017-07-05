d74e8a2f860188380fb1a23fc0f03334234bbcaf
Hirotaka Nakajima <hiro@skyblue.me.uk>
2017-07-05 21:58:34 +0900

    Add en*,em,wl,ww,p interfaces

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -293,7 +293,7 @@ class zabbix::agent (
   # can find the ipaddress of this specific interface if listenip
   # is set to for example "eth1" or "bond0.73".
   if ($listenip != undef) {
-    if ($listenip =~ /^(eth|lo|bond|lxc|eno|tap|tun|virbr|ens).*/) {
+    if ($listenip =~ /^(eth|lo|bond|lxc|en|wl|ww|em|p|tap|tun|virbr).*/) {
       $listen_ip = getvar("::ipaddress_${listenip}")
     } elsif is_ip_address($listenip) or $listenip == '*' {
       $listen_ip = $listenip
