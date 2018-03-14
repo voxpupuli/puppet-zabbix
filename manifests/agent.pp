a873f2c7a426a7570dec2d090429bee84ddfb9c6
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2018-03-14 18:23:16 +0100

    Replace listenip if/else with selector
    After some discussion this is the nicer solution.
    
    Signed-off-by: Olivier van der Toorn <oliviervdtoorn@gmail.com>

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -294,16 +294,10 @@ class zabbix::agent (
   # to network name. If more than 1 interfaces are available, we
   # can find the ipaddress of this specific interface if listenip
   # is set to for example "eth1" or "bond0.73".
-  if ($listenip != undef) {
-    if ($listenip =~ /^(eth|lo|bond|lxc|eno|tap|tun|virbr).*/) {
-      $listen_ip = getvar("::ipaddress_${listenip}")
-    } elsif is_ip_address($listenip) or $listenip == '*' {
-      $listen_ip = $listenip
-    } else {
-      $listen_ip = $::ipaddress
-    }
-  } else {
-    $listen_ip = undef
+  $listen_ip = $listenip ? {
+    /^(eth|lo|bond|lxc|eno|tap|tun|virbr).*/ => fact("networking.interfaces.${listen_ip}.ip"), # the stdlib fact function still returns undef if the fact can't be found.  This keeps the behaviour similar to how getvar would have worked, but now we use the modern networking fact instead of the legacy ipaddress_INTERFACE one
+    '*' => undef,
+    default => $listenip, # I think this is then right for all other possibilities (an ip address, or undef)
   }
 
   # So if manage_resources is set to true, we can send some data
