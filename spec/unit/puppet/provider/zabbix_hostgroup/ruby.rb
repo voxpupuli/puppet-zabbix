dcac699946b812e76fed5089f401a28ac42643de
Matthias Baur <m.baur@syseleven.de>
2019-02-22 11:58:44 +0100

    Implement self.prefetch for zabbix_hostgroup
    This is another change for #570.

---
+++ spec/unit/puppet/provider/zabbix_hostgroup/ruby.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:zabbix_hostgroup).provider(:ruby) do
+  let(:resource) do
+    Puppet::Type.type(:zabbix_hostgroup).new(
+      name: 'Testgroup'
+    )
+  end
+  let(:provider) { resource.provider }
+
+  it 'be an instance of the correct provider' do
+    expect(provider).to be_an_instance_of Puppet::Type::Zabbix_hostgroup::ProviderRuby
+  end
+
+  [:instances, :prefetch].each do |method|
+    it "should respond to the class method #{method}" do
+      expect(described_class).to respond_to(method)
+    end
+  end
+
+  [:create, :exists?, :destroy].each do |method|
+    it "should respond to the instance method #{method}" do
+      expect(described_class.new).to respond_to(method)
+    end
+  end
+end
