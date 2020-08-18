1deb298509162fe6503d3256e710403b4748924e
Ansgar Wiechers <github@planetcobalt.net>
2020-08-18 10:53:09 +0200

    have agent service provider default to fact $::service_provider

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -440,7 +440,7 @@ class zabbix::agent (
     $service_provider = 'init'
     $service_path = '/etc/rc.d/init.d'
   } else {
-    $service_provider = undef
+    $service_provider = $::service_provider
     $service_path = undef
   }
   service { $servicename:
