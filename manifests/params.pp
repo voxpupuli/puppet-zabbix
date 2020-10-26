e7641bff12c8ce43736ef8b3e10aa5d27234a19f
Will Berry <73126355+wdberry@users.noreply.github.com>
2020-10-26 10:43:03 -0400

    Update type of variable and change default value.
    Change default value of startpreprocessors from explicit declaration of implied default to undef, conditionally adding the configuration in the templates.
    
    Change type of startpreprocessors from untyped to Optional[Integer[1,1000]] to mark it as non-mandatory per Zabbix documentation and set the range of valid values also per Zabbix documentation.

--- manifests/params.pp
+++ manifests/params.pp
@@ -268,7 +268,7 @@ class zabbix::params {
   $server_startsnmptrapper                  = '0'
   $server_starttimers                       = '1'
   $server_starttrappers                     = '5'
-  $server_startpreprocessors                = '3'
+  $server_startpreprocessors                = undef
   $server_startvmwarecollectors             = '0'
   $server_timeout                           = '3'
   $server_tlscafile                         = undef
@@ -395,7 +395,7 @@ class zabbix::params {
   $proxy_startdbsyncers                     = '4'
   $proxy_startdiscoverers                   = '1'
   $proxy_starthttppollers                   = '1'
-  $proxy_startpreprocessors                 = '3'
+  $proxy_startpreprocessors                 = undef
   $proxy_startipmipollers                   = '0'
   $proxy_startjavapollers                   = '5'
   $proxy_startpingers                       = '1'
