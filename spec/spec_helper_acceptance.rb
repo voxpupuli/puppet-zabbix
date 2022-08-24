63dcee83d076fd1774a620881ad77a97202298d2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-08-24 12:54:24 +0200

    Add workaround for EL 7 locale

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
-# Managed by modulesync - DO NOT EDIT
-# https://voxpupuli.org/docs/updating-files-managed-with-modulesync/
-
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-configure_beaker
+configure_beaker do
+  # In Puppet 7 the locale ends up being C.UTF-8 if it isn't passed.
+  # This locale doesn't exist in EL7 and won't be supported either.
+  # At least PostgreSQL runs into this.
+  if host['hypervisor'] == 'docker' && host['platform'] == 'el-7-x86_64'
+    ENV['LANG'] = 'en_US.UTF-8'
+  end
+end
 
 Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
