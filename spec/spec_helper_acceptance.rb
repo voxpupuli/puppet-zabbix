b548f8446be424a856acb4cc13510a0a6fbd5003
Romain Tartière <romain@blogreen.org>
2022-09-07 11:00:12 -1000

    Make rubocop happy

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -6,9 +6,7 @@ configure_beaker do
   # In Puppet 7 the locale ends up being C.UTF-8 if it isn't passed.
   # This locale doesn't exist in EL7 and won't be supported either.
   # At least PostgreSQL runs into this.
-  if host['hypervisor'] == 'docker' && host['platform'] == 'el-7-x86_64'
-    ENV['LANG'] = 'en_US.UTF-8'
-  end
+  ENV['LANG'] = 'en_US.UTF-8' if host['hypervisor'] == 'docker' && host['platform'] == 'el-7-x86_64'
 end
 
 Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
