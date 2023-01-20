22530fae51ef6641cd4d3d73022f554002014c05
Joshua Snapp <jksnapp@ncsu.edu>
2023-01-20 17:30:52 -0500

    Provide correct gpgkey for RHEL family, version >= 9.
    Zabbix changed their gpgkey for RHEL operating systems newer than version 8. The new key works for both the "zabbix" and "zabbix_nonsupported" repos.

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -112,6 +112,35 @@ describe 'zabbix::repo' do
 
             it { is_expected.to contain_package('zabbix-required-scl-repo').with_ensure('latest').with_name('centos-release-scl') }
           end
+        when '9'
+
+          context 'on RedHat 9 and Zabbix 5.0' do
+            let :params do
+              {
+                zabbix_version: '5.0',
+                manage_repo: true
+              }
+            end
+
+            it { is_expected.to contain_yumrepo('zabbix').with_baseurl('https://repo.zabbix.com/zabbix/5.0/rhel/9/$basearch/') }
+            it { is_expected.to contain_yumrepo('zabbix').with_gpgkey('https://repo.zabbix.com/RPM-GPG-KEY-ZABBIX-08EFA7DD') }
+            it { is_expected.to contain_yumrepo('zabbix-nonsupported').with_baseurl('https://repo.zabbix.com/non-supported/rhel/9/$basearch/') }
+            it { is_expected.to contain_yumrepo('zabbix-nonsupported').with_gpgkey('https://repo.zabbix.com/RPM-GPG-KEY-ZABBIX-08EFA7DD') }
+          end
+
+          context 'on RedHat 9 and Zabbix 6.0' do
+            let :params do
+              {
+                zabbix_version: '6.0',
+                manage_repo: true
+              }
+            end
+
+            it { is_expected.to contain_yumrepo('zabbix').with_baseurl('https://repo.zabbix.com/zabbix/6.0/rhel/9/$basearch/') }
+            it { is_expected.to contain_yumrepo('zabbix').with_gpgkey('https://repo.zabbix.com/RPM-GPG-KEY-ZABBIX-08EFA7DD') }
+            it { is_expected.to contain_yumrepo('zabbix-nonsupported').with_baseurl('https://repo.zabbix.com/non-supported/rhel/9/$basearch/') }
+            it { is_expected.to contain_yumrepo('zabbix-nonsupported').with_gpgkey('https://repo.zabbix.com/RPM-GPG-KEY-ZABBIX-08EFA7DD') }
+          end
         end
       end
     end
