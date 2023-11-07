voxpupuli/puppet-zabbix

commit b1f38dd6b14233eefe80b3f3002d41c2de0410e2

Drop Zabbix 2/3/4.2/4.4 support
Only Zabbix 4.0 and 5.0/5.2 are supported upstream. This PR drops support
for older releases. See https://www.zabbix.com/de/life_cycle_and_release_policy
for current versions that upstream supports.

manifests/database/mysql.pp
@@ -27,52 +27,21 @@ class zabbix::database::mysql (
 ) inherits zabbix::params {
   assert_private()
 
-  #
-  # Adjustments for version 3.x/4.x/5.x - structure of package with sqls differs from previous versions
-  case $zabbix_version {
-    /^[345].\d+$/: {
-      if ($database_schema_path == false) or ($database_schema_path == '') {
-        $schema_path   = '/usr/share/doc/zabbix-*-mysql*'
-      }
-      else {
-        $schema_path = $database_schema_path
-      }
+  if ($database_schema_path == false) or ($database_schema_path == '') {
+    $schema_path   = '/usr/share/doc/zabbix-*-mysql*'
+  }
+  else {
+    $schema_path = $database_schema_path
+  }
 
-      case $zabbix_type {
-        'proxy': {
-          $zabbix_proxy_create_sql = "cd ${schema_path} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' -D '${database_name}' < schema.sql && touch /etc/zabbix/.schema.done"
-        }
-        default: {
-          $zabbix_server_create_sql = "cd ${schema_path} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' -D '${database_name}' < create.sql && touch /etc/zabbix/.schema.done"
-          $zabbix_server_images_sql = 'touch /etc/zabbix/.images.done'
-          $zabbix_server_data_sql   = 'touch /etc/zabbix/.data.done'
-        }
-      }
+  case $zabbix_type {
+    'proxy': {
+      $zabbix_proxy_create_sql = "cd ${schema_path} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' -D '${database_name}' < schema.sql && touch /etc/zabbix/.schema.done"
     }
     default: {
-      if ($database_schema_path == false) or ($database_schema_path == '') {
-        case $facts['os']['family'] {
-          'RedHat': {
-            $schema_path   = "/usr/share/doc/zabbix-*-mysql-${zabbix_version}*/create"
-          }
-          default : {
-            $schema_path   = '/usr/share/zabbix-*-mysql'
-          }
-        }
-      }
-      else {
-        $schema_path = $database_schema_path
-      }
-      case $zabbix_type {
-        'proxy': {
-          $zabbix_proxy_create_sql = "cd ${schema_path} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' -D '${database_name}' < schema.sql && touch /etc/zabbix/.schema.done"
-        }
-        default: {
-          $zabbix_server_create_sql = "cd ${schema_path} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' -D '${database_name}' < schema.sql && touch /etc/zabbix/.schema.done"
-          $zabbix_server_images_sql = "cd ${schema_path} && if [ -f images.sql.gz ]; then gunzip -f images.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' -D '${database_name}' < images.sql && touch /etc/zabbix/.images.done"
-          $zabbix_server_data_sql   = "cd ${schema_path} && if [ -f data.sql.gz ]; then gunzip -f data.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' -D '${database_name}' < data.sql && touch /etc/zabbix/.data.done"
-        }
-      }
+      $zabbix_server_create_sql = "cd ${schema_path} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' -D '${database_name}' < create.sql && touch /etc/zabbix/.schema.done"
+      $zabbix_server_images_sql = 'touch /etc/zabbix/.images.done'
+      $zabbix_server_data_sql   = 'touch /etc/zabbix/.data.done'
     }
   }
 

manifests/database/postgresql.pp
@@ -27,59 +27,27 @@ class zabbix::database::postgresql (
 ) inherits zabbix::params {
   assert_private()
 
-  #
-  # Adjustments for version 3.x/4.x/5.x - structure of package with sqls differs from previous versions
-  case $zabbix_version {
-    /^[345].\d+$/: {
-      if ($database_schema_path == false) or ($database_schema_path == '') {
-        case $facts['os']['name'] {
-          'CentOS', 'RedHat', 'OracleLinux', 'VirtuozzoLinux': {
-            $schema_path   = "/usr/share/doc/zabbix-*-pgsql-${zabbix_version}*/"
-          }
-          default : {
-            $schema_path   = '/usr/share/doc/zabbix-*-pgsql'
-          }
-        }
+  if ($database_schema_path == false) or ($database_schema_path == '') {
+    case $facts['os']['name'] {
+      'CentOS', 'RedHat', 'OracleLinux', 'VirtuozzoLinux': {
+        $schema_path   = "/usr/share/doc/zabbix-*-pgsql-${zabbix_version}*/"
       }
-      else {
-        $schema_path = $database_schema_path
-      }
-
-      case $zabbix_type {
-        'proxy': {
-          $zabbix_proxy_create_sql = "cd ${schema_path} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -d '${database_name}' -f schema.sql && touch /etc/zabbix/.schema.done"
-        }
-        default: {
-          $zabbix_server_create_sql = "cd ${schema_path} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -d '${database_name}' -f create.sql && touch /etc/zabbix/.schema.done"
-          $zabbix_server_images_sql = 'touch /etc/zabbix/.images.done'
-          $zabbix_server_data_sql   = 'touch /etc/zabbix/.data.done'
-        }
+      default : {
+        $schema_path   = '/usr/share/doc/zabbix-*-pgsql'
       }
     }
+  } else {
+    $schema_path = $database_schema_path
+  }
+
+  case $zabbix_type {
+    'proxy': {
+      $zabbix_proxy_create_sql = "cd ${schema_path} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -d '${database_name}' -f schema.sql && touch /etc/zabbix/.schema.done"
+    }
     default: {
-      if ($database_schema_path == false) or ($database_schema_path == '') {
-        case $facts['os']['name'] {
-          'CentOS', 'RedHat', 'OracleLinux', 'VirtuozzoLinux': {
-            $schema_path   = "/usr/share/doc/zabbix-*-pgsql-${zabbix_version}*/create"
-          }
-          default : {
-            $schema_path   = '/usr/share/zabbix-*-pgsql'
-          }
-        }
-      }
-      else {
-        $schema_path = $database_schema_path
-      }
-      case $zabbix_type {
-        'proxy': {
-          $zabbix_proxy_create_sql = "cd ${schema_path} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -d '${database_name}' -f schema.sql && touch /etc/zabbix/.schema.done"
-        }
-        default: {
-          $zabbix_server_create_sql = "cd ${schema_path} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -d '${database_name}' -f schema.sql && touch /etc/zabbix/.schema.done"
-          $zabbix_server_images_sql = "cd ${schema_path} && if [ -f images.sql.gz ]; then gunzip -f images.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -d '${database_name}' -f images.sql && touch /etc/zabbix/.images.done"
-          $zabbix_server_data_sql   = "cd ${schema_path} && if [ -f data.sql.gz ]; then gunzip -f data.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -d '${database_name}' -f data.sql && touch /etc/zabbix/.data.done"
-        }
-      }
+      $zabbix_server_create_sql = "cd ${schema_path} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -d '${database_name}' -f create.sql && touch /etc/zabbix/.schema.done"
+      $zabbix_server_images_sql = 'touch /etc/zabbix/.images.done'
+      $zabbix_server_data_sql   = 'touch /etc/zabbix/.data.done'
     }
   }
 

spec/classes/database_mysql_spec.rb
@@ -21,54 +21,10 @@ describe 'zabbix::database::mysql' do
           is_expected.not_to compile.with_all_deps
         end
       end
-      path2 = if facts[:osfamily] == 'RedHat'
-                # path to sql files on zabbix 2.4 on RedHat
-                '/usr/share/doc/zabbix-*-mysql-2.4*/create'
-              else
-                # path to sql files on zabbix 2.4 on Debian
-                '/usr/share/zabbix-*-mysql'
-              end
       # path to sql files on zabbix 3.X on Debian and RedHat
-      path3 = '/usr/share/doc/zabbix-*-mysql*'
+      path = '/usr/share/doc/zabbix-*-mysql*'
 
-      context 'when zabbix_type is server and zabbix version is 2.4' do
-        let :params do
-          {
-            database_name: 'zabbix-server',
-            database_user: 'zabbix-server',
-            database_password: 'zabbix-server',
-            database_host: 'rspec.puppet.com',
-            zabbix_type: 'server',
-            zabbix_version: '2.4'
-          }
-        end
-
-        it { is_expected.to contain_class('zabbix::database::mysql') }
-        it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path2} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-server' -p'zabbix-server' -D 'zabbix-server' < schema.sql && touch /etc/zabbix/.schema.done") }
-        it { is_expected.to contain_exec('zabbix_server_images.sql').with_command("cd #{path2} && if [ -f images.sql.gz ]; then gunzip -f images.sql.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-server' -p'zabbix-server' -D 'zabbix-server' < images.sql && touch /etc/zabbix/.images.done") }
-        it { is_expected.to contain_exec('zabbix_server_data.sql').with_command("cd #{path2} && if [ -f data.sql.gz ]; then gunzip -f data.sql.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-server' -p'zabbix-server' -D 'zabbix-server' < data.sql && touch /etc/zabbix/.data.done") }
-        it { is_expected.to contain_class('zabbix::params') }
-      end
-
-      describe 'when zabbix_type is proxy and zabbix version is 2.4' do
-        let :params do
-          {
-            database_name: 'zabbix-proxy',
-            database_user: 'zabbix-proxy',
-            database_password: 'zabbix-proxy',
-            database_host: 'rspec.puppet.com',
-            zabbix_type: 'proxy',
-            zabbix_version: '2.4'
-          }
-        end
-
-        it { is_expected.to contain_class('zabbix::database::mysql') }
-        it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_exec('zabbix_proxy_create.sql').with_command("cd #{path2} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-proxy' -p'zabbix-proxy' -D 'zabbix-proxy' < schema.sql && touch /etc/zabbix/.schema.done") }
-      end
-
-      %w[3.0 3.2 3.4 4.0 4.2 4.4 5.0 5.2].each do |zabbix_version|
+      %w[4.0 5.0 5.2].each do |zabbix_version|
         context "when zabbix_type is server and zabbix version is #{zabbix_version}" do
           let :params do
             {
@@ -83,7 +39,7 @@ describe 'zabbix::database::mysql' do
 
           it { is_expected.to contain_class('zabbix::database::mysql') }
           it { is_expected.to compile.with_all_deps }
-          it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path3} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-server' -p'zabbix-server' -D 'zabbix-server' < create.sql && touch /etc/zabbix/.schema.done") }
+          it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-server' -p'zabbix-server' -D 'zabbix-server' < create.sql && touch /etc/zabbix/.schema.done") }
           it { is_expected.to contain_exec('zabbix_server_images.sql').with_command('touch /etc/zabbix/.images.done') }
           it { is_expected.to contain_exec('zabbix_server_data.sql').with_command('touch /etc/zabbix/.data.done') }
 
@@ -101,7 +57,7 @@ describe 'zabbix::database::mysql' do
 
             it { is_expected.to contain_class('zabbix::database::mysql') }
             it { is_expected.to compile.with_all_deps }
-            it { is_expected.to contain_exec('zabbix_proxy_create.sql').with_command("cd #{path3} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-proxy' -p'zabbix-proxy' -D 'zabbix-proxy' < schema.sql && touch /etc/zabbix/.schema.done") }
+            it { is_expected.to contain_exec('zabbix_proxy_create.sql').with_command("cd #{path} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-proxy' -p'zabbix-proxy' -D 'zabbix-proxy' < schema.sql && touch /etc/zabbix/.schema.done") }
           end
         end
       end

spec/classes/database_postgresql_spec.rb
@@ -25,7 +25,7 @@ describe 'zabbix::database::postgresql' do
         path_for2 = '/usr/share/zabbix-*-pgsql'
       end
 
-      %w[2.4 3.2 3.4 4.0 4.2 4.4 5.0 5.2].each do |zabbix_version|
+      %w[4.0 5.0 5.2].each do |zabbix_version|
         case facts[:os]['name']
         when 'CentOS', 'RedHat', 'OracleLinux', 'VirtuozzoLinux'
           # Path for versions >= 3.x on RedHat
@@ -49,15 +49,9 @@ describe 'zabbix::database::postgresql' do
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_exec('update_pgpass').with_command('echo node01.example.com:5432:zabbix-server:zabbix-server:zabbix-server >> /root/.pgpass') }
-          if zabbix_version != '2.4'
-            it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path_for3_and_up} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-server' -d 'zabbix-server' -f create.sql && touch /etc/zabbix/.schema.done") }
-            it { is_expected.to contain_exec('zabbix_server_images.sql').with_command('touch /etc/zabbix/.images.done') }
-            it { is_expected.to contain_exec('zabbix_server_data.sql').with_command('touch /etc/zabbix/.data.done') }
-          else
-            it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path_for2} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-server' -d 'zabbix-server' -f schema.sql && touch /etc/zabbix/.schema.done") }
-            it { is_expected.to contain_exec('zabbix_server_images.sql').with_command("cd #{path_for2} && if [ -f images.sql.gz ]; then gunzip -f images.sql.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-server' -d 'zabbix-server' -f images.sql && touch /etc/zabbix/.images.done") }
-            it { is_expected.to contain_exec('zabbix_server_data.sql').with_command("cd #{path_for2} && if [ -f data.sql.gz ]; then gunzip -f data.sql.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-server' -d 'zabbix-server' -f data.sql && touch /etc/zabbix/.data.done") }
-          end
+          it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path_for3_and_up} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-server' -d 'zabbix-server' -f create.sql && touch /etc/zabbix/.schema.done") }
+          it { is_expected.to contain_exec('zabbix_server_images.sql').with_command('touch /etc/zabbix/.images.done') }
+          it { is_expected.to contain_exec('zabbix_server_data.sql').with_command('touch /etc/zabbix/.data.done') }
           it { is_expected.to contain_file('/root/.pgpass') }
           it { is_expected.to contain_class('zabbix::params') }
         end
@@ -76,11 +70,7 @@ describe 'zabbix::database::postgresql' do
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_exec('update_pgpass').with_command('echo node01.example.com:5432:zabbix-proxy:zabbix-proxy:zabbix-proxy >> /root/.pgpass') }
-          if zabbix_version != '2.4'
-            it { is_expected.to contain_exec('zabbix_proxy_create.sql').with_command("cd #{path_for3_and_up} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-proxy' -d 'zabbix-proxy' -f schema.sql && touch /etc/zabbix/.schema.done") }
-          else
-            it { is_expected.to contain_exec('zabbix_proxy_create.sql').with_command("cd #{path_for2} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-proxy' -d 'zabbix-proxy' -f schema.sql && touch /etc/zabbix/.schema.done") }
-          end
+          it { is_expected.to contain_exec('zabbix_proxy_create.sql').with_command("cd #{path_for3_and_up} && if [ -f schema.sql.gz ]; then gunzip -f schema.sql.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-proxy' -d 'zabbix-proxy' -f schema.sql && touch /etc/zabbix/.schema.done") }
           it { is_expected.to contain_class('zabbix::params') }
         end
       end

commit d5a665b844cd08bf863cdad199ebb4ff96f960e8

Drop unused zabbix_server_{images,data}_sql
Fixes: b1f38dd6b142 ("Drop Zabbix 2/3/4.2/4.4 support")

manifests/database/mysql.pp
@@ -52,8 +52,6 @@ class zabbix::database::mysql (
         true  => "cd ${schema_path} && if [ -f server.sql.gz ]; then gunzip -f server.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' ${port}-D '${database_name}' < server.sql && touch /etc/zabbix/.schema.done",
         false => "cd ${schema_path} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && mysql -h '${database_host}' -u '${database_user}' -p'${database_password}' ${port}-D '${database_name}' < create.sql && touch /etc/zabbix/.schema.done"
       }
-      $zabbix_server_images_sql = 'touch /etc/zabbix/.images.done'
-      $zabbix_server_data_sql   = 'touch /etc/zabbix/.data.done'
     }
   }
 
@@ -72,18 +70,6 @@ class zabbix::database::mysql (
         command  => $zabbix_server_create_sql,
         path     => "/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:${database_path}",
         unless   => 'test -f /etc/zabbix/.schema.done',
-        provider => 'shell',
-      }
-      -> exec { 'zabbix_server_images.sql':
-        command  => $zabbix_server_images_sql,
-        path     => "/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:${database_path}",
-        unless   => 'test -f /etc/zabbix/.images.done',
-        provider => 'shell',
-      }
-      -> exec { 'zabbix_server_data.sql':
-        command  => $zabbix_server_data_sql,
-        path     => "/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:${database_path}",
-        unless   => 'test -f /etc/zabbix/.data.done',
         provider => 'shell',
       }
     }

manifests/database/postgresql.pp
@@ -53,8 +53,6 @@ class zabbix::database::postgresql (
         true  => "cd ${schema_path} && if [ -f server.sql.gz ]; then gunzip -f server.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -p ${database_port} -d '${database_name}' -f server.sql && touch /etc/zabbix/.schema.done",
         false => "cd ${schema_path} && if [ -f create.sql.gz ]; then gunzip -f create.sql.gz ; fi && psql -h '${database_host}' -U '${database_user}' -p ${database_port} -d '${database_name}' -f create.sql && touch /etc/zabbix/.schema.done"
       }
-      $zabbix_server_images_sql = 'touch /etc/zabbix/.images.done'
-      $zabbix_server_data_sql   = 'touch /etc/zabbix/.data.done'
     }
   }
 
@@ -87,20 +85,6 @@ class zabbix::database::postgresql (
         command  => $zabbix_server_create_sql,
         path     => "/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:${database_path}",
         unless   => 'test -f /etc/zabbix/.schema.done',
-        provider => 'shell',
-        require  => Exec['update_pgpass'],
-      }
-      -> exec { 'zabbix_server_images.sql':
-        command  => $zabbix_server_images_sql,
-        path     => "/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:${database_path}",
-        unless   => 'test -f /etc/zabbix/.images.done',
-        provider => 'shell',
-        require  => Exec['update_pgpass'],
-      }
-      -> exec { 'zabbix_server_data.sql':
-        command  => $zabbix_server_data_sql,
-        path     => "/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:${database_path}",
-        unless   => 'test -f /etc/zabbix/.data.done',
         provider => 'shell',
         require  => Exec['update_pgpass'],
       }

spec/classes/database_mysql_spec.rb
@@ -54,8 +54,6 @@ describe 'zabbix::database::mysql' do
             it { is_expected.to contain_class('zabbix::database::mysql') }
             it { is_expected.to compile.with_all_deps }
             it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path} && if [ -f #{sql_server}.gz ]; then gunzip -f #{sql_server}.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-server' -p'zabbix-server' -P 3306 -D 'zabbix-server' < #{sql_server} && touch /etc/zabbix/.schema.done") }
-            it { is_expected.to contain_exec('zabbix_server_images.sql').with_command('touch /etc/zabbix/.images.done') }
-            it { is_expected.to contain_exec('zabbix_server_data.sql').with_command('touch /etc/zabbix/.data.done') }
           end
 
           describe 'and no database_port is defined' do
@@ -74,8 +72,6 @@ describe 'zabbix::database::mysql' do
             it { is_expected.to contain_class('zabbix::database::mysql') }
             it { is_expected.to compile.with_all_deps }
             it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path} && if [ -f #{sql_server}.gz ]; then gunzip -f #{sql_server}.gz ; fi && mysql -h 'rspec.puppet.com' -u 'zabbix-server' -p'zabbix-server' -D 'zabbix-server' < #{sql_server} && touch /etc/zabbix/.schema.done") }
-            it { is_expected.to contain_exec('zabbix_server_images.sql').with_command('touch /etc/zabbix/.images.done') }
-            it { is_expected.to contain_exec('zabbix_server_data.sql').with_command('touch /etc/zabbix/.data.done') }
           end
         end
 

spec/classes/database_postgresql_spec.rb
@@ -63,8 +63,6 @@ describe 'zabbix::database::postgresql' do
             it { is_expected.to compile.with_all_deps }
             it { is_expected.to contain_exec('update_pgpass').with_command('echo node01.example.com:5432:zabbix-server:zabbix-server:zabbix-server >> /root/.pgpass') }
             it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path} && if [ -f #{sql_server}.gz ]; then gunzip -f #{sql_server}.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-server' -p 5432 -d 'zabbix-server' -f #{sql_server} && touch /etc/zabbix/.schema.done") }
-            it { is_expected.to contain_exec('zabbix_server_images.sql').with_command('touch /etc/zabbix/.images.done') }
-            it { is_expected.to contain_exec('zabbix_server_data.sql').with_command('touch /etc/zabbix/.data.done') }
             it { is_expected.to contain_file('/root/.pgpass') }
             it { is_expected.to contain_class('zabbix::params') }
 
@@ -74,8 +72,6 @@ describe 'zabbix::database::postgresql' do
               it { is_expected.to compile.with_all_deps }
               it { is_expected.to contain_exec('update_pgpass').with_command('echo node01.example.com:6432:zabbix-server:zabbix-server:zabbix-server >> /root/.pgpass') }
               it { is_expected.to contain_exec('zabbix_server_create.sql').with_command("cd #{path} && if [ -f #{sql_server}.gz ]; then gunzip -f #{sql_server}.gz ; fi && psql -h 'node01.example.com' -U 'zabbix-server' -p 6432 -d 'zabbix-server' -f #{sql_server} && touch /etc/zabbix/.schema.done") }
-              it { is_expected.to contain_exec('zabbix_server_images.sql').with_command('touch /etc/zabbix/.images.done') }
-              it { is_expected.to contain_exec('zabbix_server_data.sql').with_command('touch /etc/zabbix/.data.done') }
               it { is_expected.to contain_file('/root/.pgpass') }
               it { is_expected.to contain_class('zabbix::params') }
             end

spec/classes/server_spec.rb
@@ -95,8 +95,6 @@ describe 'zabbix::server' do
         it { is_expected.to contain_package('zabbix-server-mysql').with_name('zabbix-server-mysql') }
         it { is_expected.to contain_file('/etc/zabbix/zabbix_server.conf').with_require('Package[zabbix-server-mysql]') }
         it { is_expected.to contain_exec('zabbix_server_create.sql') }
-        it { is_expected.to contain_exec('zabbix_server_data.sql') }
-        it { is_expected.to contain_exec('zabbix_server_images.sql') }
       end
 
       # Include directory should be available.