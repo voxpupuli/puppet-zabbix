f3d5fb35fd62962ccdd86f41ff026283cadc7721
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-12-29 13:44:56 +0100

    Properly format prepare_host
    This uses correct heredoc descriptions and directly passes them into the
    right methods, without additional variables.

--- spec/support/acceptance/prepare_host.rb
+++ spec/support/acceptance/prepare_host.rb
@@ -5,34 +5,31 @@ def prepare_host
     shell('yum clean all --verbose; rm -rf /etc/yum.repos.d/Zabbix*.repo')
   end
 
-  cleanup_puppet = <<-SHELL
-      $services = $facts['os']['family'] ? {
-        'RedHat' => ['zabbix-server', 'httpd'],
-        'Debian' => ['zabbix-server', 'apache2'],
-        default  => [],
-      }
-      service { $services:
-        ensure => stopped
-      }
+  apply_manifest <<~PUPPET
+    $services = $facts['os']['family'] ? {
+      'RedHat' => ['zabbix-server', 'httpd'],
+      'Debian' => ['zabbix-server', 'apache2'],
+      default  => [],
+    }
+    service { $services:
+      ensure => stopped
+    }
 
-      $packages = $facts['os']['family'] ? {
-        'RedHat' => ['zabbix-server-pgsql', 'zabbix-server-pgsql-scl', 'zabbix-web', 'zabbix-web-pgsql', 'zabbix-web-pgsql-scl', 'zabbix-frontend-php', 'zabbix-sql-scripts'],
-        'Debian' => ['zabbix-server-pgsql', 'zabbix-web-pgsql', 'zabbix-frontend-php', 'zabbix-sql-scripts'],
-        default  => [],
-      }
-      package { $packages:
-        ensure => purged
-      }
+    $packages = $facts['os']['family'] ? {
+      'RedHat' => ['zabbix-server-pgsql', 'zabbix-server-pgsql-scl', 'zabbix-web', 'zabbix-web-pgsql', 'zabbix-web-pgsql-scl', 'zabbix-frontend-php', 'zabbix-sql-scripts'],
+      'Debian' => ['zabbix-server-pgsql', 'zabbix-web-pgsql', 'zabbix-frontend-php', 'zabbix-sql-scripts'],
+      default  => [],
+    }
+    package { $packages:
+      ensure => purged
+    }
+  PUPPET
+
+  shell <<~SHELL
+    /opt/puppetlabs/puppet/bin/gem uninstall zabbixapi -a
+    rm -f /etc/zabbix/.*done
+    if id postgres > /dev/null 2>&1; then
+      su - postgres -c "psql -c 'drop database if exists zabbix_server;'"
+    fi
   SHELL
-
-  cleanup_script = <<-SHELL
-  /opt/puppetlabs/puppet/bin/gem uninstall zabbixapi -a
-  rm -f /etc/zabbix/.*done
-  if id postgres > /dev/null 2>&1; then
-    su - postgres -c "psql -c 'drop database if exists zabbix_server;'"
-  fi
-  SHELL
-
-  apply_manifest(cleanup_puppet)
-  shell(cleanup_script)
 end
