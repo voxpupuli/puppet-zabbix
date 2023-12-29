b4837d3b9d79d7ed4212980b0315cb688913f0a2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-12-29 13:41:09 +0100

    Move node setup from prepare_host to setup_acceptance_node.pp
    This is setup code only needs to happen once. By using
    setup_acceptance_node.pp we can rely on editors to use proper syntax
    highlighting and use lint to validate our style.

---
+++ spec/setup_acceptance_node.pp
@@ -0,0 +1,33 @@
+case $facts['os']['name'] {
+  'Debian': {
+    # On Debian it seems that make is searching for mkdir in /usr/bin/ but mkdir
+    # does not exist. Symlink it from /bin/mkdir to make it work.
+    file { '/usr/bin/mkdir':
+      ensure => link,
+      target => '/bin/mkdir',
+    }
+  }
+  'Ubuntu': {
+    # The Ubuntu 18.04+ docker image has a dpkg config that won't install docs, to keep used space low
+    # zabbix packages their SQL file as doc, we need that for bootstrapping the database
+    file { '/etc/dpkg/dpkg.cfg.d/excludes':
+      ensure => absent,
+    }
+  }
+  default: {}
+}
+
+case $facts['os']['family'] {
+  'RedHat': {
+    if $facts['os']['release']['major'] == '7' {
+      # The CentOS docker image has a yum config that won't install docs, to keep used space low
+      # zabbix packages their SQL file as doc, we need that for bootstrapping the database
+      augeas { 'remove tsflags=nodocs from yum.conf':
+        changes => [
+          'rm /files/etc/yum.conf/main/tsflags',
+        ],
+      }
+    }
+  }
+  default: {}
+}
